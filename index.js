af1098949d45f2f02066148eb3d65f92f937f4e4
Kathy <kleboeuf@townisp.com>
2018-05-28 20:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine) {
   let n = 0; 
   let string = 'The line is currently: '
   
-  if(katzDeliLine.length == 0){
+  if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }
   for(let i = 0; i < katzDeliLine.length; i++) {
